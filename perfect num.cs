f2948afd7983b8fb2b70955b332f910979052d5d
mahaahmeddd <121254760+mahaahmeddd@users.noreply.github.com>
2022-12-22 21:13:11 +0200

    Add files via upload

---
+++ perfect num.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int X, Y;
+            Console.Write("Enter the Start Number : ");
+            X=int.Parse(Console.ReadLine());
+            Console.Write("Enter the End number");
+            Y = int.Parse(Console.ReadLine());
+            for (int a = X; a < Y; a++)
+            {
+                int sum = 0;
+            for (int b = 1; b < a; b++)
+             {
+                    if ( a % b == 0)
+                        sum = sum + b;
+                }
+                if (sum == 0)
+                    continue;
+              if( a == sum)
+                    
+                        Console.WriteLine("{0} is Perfect Number",a);
+
+
+
+            }
+
+        }
+    }
+}
---
+++ prime num.cs
@@ -0,0 +1,31 @@
+﻿namespace prime_num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Start number : ");
+            int X = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the end number : ");
+            int Y = int.Parse(Console.ReadLine());
+
+            for (int a = X; a <= Y; a++)
+            {
+                int c = 0;
+                for (int b = 1; b <= a; b++)
+                {
+                    if (b == 1 || a == b)
+                        c++;
+
+                    else if (a % b == 0)
+                        break;
+
+                }
+                if (c == 2)
+                    Console.WriteLine("{0} is prime Number", a);
+            }
+        }
+    }
+}
+        
